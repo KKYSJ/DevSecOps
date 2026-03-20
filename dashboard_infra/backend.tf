80d832553a35e8404d3525ba95f1aad8d3bf0ecc
Sun0510 <sunhyung5432@naver.com>
2026-03-20 14:12:49 +0900

    Configure remote Terraform backend

--- dashboard_infra/backend.tf
+++ dashboard_infra/backend.tf
@@ -1,6 +1,9 @@
-# 권장: 실제 운영에서는 로컬 상태 대신 S3 + DynamoDB(or S3 native locking) 원격 상태를 사용하세요.
-# 아래는 예시이며, bucket/key/region은 `terraform init -backend-config=...` 로 주입하는 방식을 추천합니다.
-#
-# terraform {
-#   backend "s3" {}
-# }
+terraform {
+  backend "s3" {
+    bucket         = "secureflow-terraform-state-213026893250"
+    key            = "dashboard_infra/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "secureflow-terraform-locks"
+    encrypt        = true
+  }
+}
